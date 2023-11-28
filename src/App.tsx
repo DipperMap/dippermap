13ad387c0df1c88c101fd7dbd6c6cec1d0958677
lzxue <120635640@qq.com>
2023-11-28 14:28:27 +0800

    docs: 更新名称

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DipperMap
+          DipperMap 星辰地图网站导航
         </p>
       </header>
     </div>
