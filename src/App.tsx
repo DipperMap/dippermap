7c1d2e7e24c90b8dda24905f722fae7cff5403c9
lzxue <120635640@qq.com>
2023-11-28 14:11:17 +0800

    chore: 内容文案

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          DipperMap
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
