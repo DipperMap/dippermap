09c3f07fa70d223daddaf42ed43106f358128792
syb01094648 <syb01094648@antgroup.com>
2023-11-30 16:51:09 +0800

    fix: header左侧新增微信和gitHub

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Layout, Button } from "antd";
 import "./App.css";
 import { AppCard } from "./components/app-content";
 import { AppSider } from "./components/app-sider";
-import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import classNames from "classnames";
+import { AppHeader } from "./components/app-header";
 const { Header, Footer, Sider, Content } = Layout;
 function App() {
   const [collapsed, setCollapsed] = useState(false);
@@ -20,9 +20,7 @@ function App() {
       </Sider>
       <Layout style={{ marginLeft: collapsed ? 0 : 270 }}>
         <Header className="App-header">
-          <div onClick={() => setCollapsed(!collapsed)}>
-            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-          </div>
+          <AppHeader collapsed={collapsed} setCollapsed={setCollapsed} />
         </Header>
         <Content className="App-content">
           <AppCard />
---
+++ src/components/app-header/index.tsx
@@ -0,0 +1,57 @@
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  WechatOutlined,
+  GithubOutlined,
+} from "@ant-design/icons";
+import "./index.css";
+import { Tooltip, Divider } from "antd";
+import weChat from "../../assets/weixin.jpeg";
+
+type AppHeaderProps = {
+  collapsed: boolean;
+  setCollapsed: (val: boolean) => void;
+};
+
+export const AppHeader: React.FC<AppHeaderProps> = ({
+  setCollapsed,
+  collapsed,
+}) => {
+  return (
+    <div className="app-header">
+      <div onClick={() => setCollapsed(!collapsed)}>
+        {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+      </div>
+      <div className="header-right">
+        <div className="weChat">
+          <Tooltip
+            color={"#fff"}
+            placement="bottom"
+            title={
+              <div>
+                <div className="weChatText">
+                  <span>微信扫一扫关注</span>
+                </div>
+                <img
+                  style={{ paddingTop: 6 }}
+                  width={130}
+                  src={weChat}
+                  alt=""
+                />
+              </div>
+            }
+          >
+            <WechatOutlined />
+          </Tooltip>
+        </div>
+        <div className="gitHub">
+          <GithubOutlined
+            onClick={() => {
+              window.open("https://github.com/DipperMap/dippermap");
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
