76fe144b08653a21cff4bc6c8ed2bda67370f445
Shiqi <wsq_star@outlook.com>
2023-12-23 11:17:22 +0800

    chore: 🔧week2

--- src/data/sites/tech/groups/weekly/week2.ts
+++ src/data/sites/tech/groups/weekly/week2.ts
@@ -10,7 +10,7 @@ export const Week2: IGroup = {
       name: 'Leafmap',
       en_name: 'Leafmap',
       description:
-        'Leafmap is a Python library that simplifies GIS data visualization and analysis. It supports various interactive maps and seamlessly integrates geographic data from different sources, allowing users to create expressive maps quickly without the need for complex code.',
+        'Leafmap是一个Python库，简化了GIS数据的可视化和分析。支持多种交互式地图，可以轻松集成不同源的地理数据，使用户能够快速创建富有表现力的地图，无需编写复杂的代码。',
       en_description:
         'Leafmap is a Python library that simplifies GIS data visualization and analysis. It supports various interactive maps and seamlessly integrates geographic data from different sources, allowing users to create expressive maps quickly without the need for complex code.',
       site_url: 'https://leafmap.org/',
@@ -22,7 +22,7 @@ export const Week2: IGroup = {
       name: 'DuckDB',
       en_name: 'DuckDB',
       description:
-        'DuckDB is an OLAP (Online Analytical Processing) analytical SQL database. It is particularly suitable for data science and complex analysis, directly usable in the Python environment. DuckDB is closely integrated with the Pandas library and efficiently handles large-scale datasets, making it well-suited for geographic data analysis.',
+        'DuckDB是一个适用于OLAP的分析型SQL数据库，特别适合数据科学和复杂分析。与Pandas库紧密集成，可以高效处理大规模数据集，支持快速查询与数据操作，尤其适合地理数据分析。',
       en_description:
         'DuckDB is an OLAP (Online Analytical Processing) analytical SQL database. It is particularly suitable for data science and complex analysis, directly usable in the Python environment. DuckDB is closely integrated with the Pandas library and efficiently handles large-scale datasets, making it well-suited for geographic data analysis.',
       site_url: 'https://duckdb.org/',
@@ -34,7 +34,7 @@ export const Week2: IGroup = {
       name: 'geemap',
       en_name: 'geemap',
       description:
-        "geemap is a Python library that interacts with Google Earth Engine (GEE). It leverages GEE's powerful geospatial data processing capabilities, enabling users to perform complex geospatial analysis in the Python environment. geemap's functionalities include map creation, map algebra, and importing/exporting various spatial data.",
+        'geemap是一个与Google Earth Engine（GEE）交互的Python库，利用GEE的地理数据处理能力进行复杂的地理空间分析。功能包括地图制作、地图代数，以及多种空间数据的导入和导出。',
       en_description:
         "geemap is a Python library that interacts with Google Earth Engine (GEE). It leverages GEE's powerful geospatial data processing capabilities, enabling users to perform complex geospatial analysis in the Python environment. geemap's functionalities include map creation, map algebra, and importing/exporting various spatial data.",
       site_url: 'https://geemap.org/',
@@ -46,7 +46,7 @@ export const Week2: IGroup = {
       name: 'Placekey',
       en_name: 'Placekey',
       description:
-        'Placekey is an innovative geographic coding system designed to provide a unified way to identify physical locations. It offers a unique identifier for locations, greatly simplifying the management and analysis of location data. When dealing with geospatial datasets, Placekey serves as a bridge to connect different data sources, providing great convenience for geographic data analysis.',
+        'Placekey是一个创新的地理编码系统，旨在提供统一的方式来识别物理地点。为地点提供了一个独特的标识符，简化了地点数据的管理和分析工作，可作为连接不同数据源的桥梁。',
       en_description:
         'Placekey is an innovative geographic coding system designed to provide a unified way to identify physical locations. It offers a unique identifier for locations, greatly simplifying the management and analysis of location data. When dealing with geospatial datasets, Placekey serves as a bridge to connect different data sources, providing great convenience for geographic data analysis.',
       site_url: 'https://www.placekey.io/',
@@ -58,7 +58,7 @@ export const Week2: IGroup = {
       name: 'Apache Sedona',
       en_name: 'Apache Sedona',
       description:
-        'Apache Sedona (formerly GeoSpark) is a distributed geospatial database that supports large-scale geographic data processing on Apache Spark. It provides geospatial RDDs and DataFrames, allowing developers to process massive amounts of geospatial information in parallel. Suitable for complex spatial data analysis and processing tasks.',
+        'Apache Sedona（前身为GeoSpark）是一个分布式地理空间数据库，支持在Apache Spark上进行大规模地理数据处理。提供地理空间RDDs和DataFrames，适用于复杂的空间数据分析和处理任务。',
       en_description:
         'Apache Sedona (formerly GeoSpark) is a distributed geospatial database that supports large-scale geographic data processing on Apache Spark. It provides geospatial RDDs and DataFrames, allowing developers to process massive amounts of geospatial information in parallel. Suitable for complex spatial data analysis and processing tasks.',
       site_url: 'https://sedona.apache.org/',
@@ -70,7 +70,7 @@ export const Week2: IGroup = {
       name: 'xarray',
       en_name: 'xarray',
       description:
-        'xarray is a Python package for flexible handling of labeled multi-dimensional arrays. It builds upon native NumPy arrays, adding dimensions, coordinates, and attribute labels to make data operations more intuitive. Widely used in fields such as meteorology, oceanography, and climatology.',
+        'xarray是一个处理带标签的多维数组的Python包，基于NumPy数组，增加了维度、坐标和属性标签，广泛应用于气象学、海洋学和气候学等领域。',
       en_description:
         'xarray is a Python package for flexible handling of labeled multi-dimensional arrays. It builds upon native NumPy arrays, adding dimensions, coordinates, and attribute labels to make data operations more intuitive. Widely used in fields such as meteorology, oceanography, and climatology.',
       site_url: 'http://xarray.pydata.org/en/stable/',
@@ -82,7 +82,7 @@ export const Week2: IGroup = {
       name: 'GeoPandas',
       en_name: 'GeoPandas',
       description:
-        'GeoPandas is an open-source library that extends the functionality of pandas to handle geospatial data. It simplifies spatial operations in Python, widely used in environmental planning, GIS data processing, and spatial analysis.',
+        'GeoPandas是一个扩展了pandas功能以处理地理空间数据的开源库。简化了在Python中的空间操作，广泛用于环境规划、GIS数据处理和空间分析。',
       en_description:
         'GeoPandas is an open-source library that extends the functionality of pandas to handle geospatial data. It simplifies spatial operations in Python, widely used in environmental planning, GIS data processing, and spatial analysis.',
       site_url: 'https://geopandas.org/',
@@ -94,7 +94,7 @@ export const Week2: IGroup = {
       name: 'Pydeck',
       en_name: 'Pydeck',
       description:
-        'Pydeck is a high-performance, WebGL-based visualization tool. It serves as the Python interface to deck.gl and is used for interactive display of large-scale datasets, especially suitable for urban planning and geographic spatial analysis.',
+        'Pydeck是一个基于WebGL的高效可视化工具，是deck.gl的Python接口，用于交互式地展示大规模数据集，特别适用于城市规划和地理空间分析。',
       en_description:
         'Pydeck is a high-performance, WebGL-based visualization tool. It serves as the Python interface to deck.gl and is used for interactive display of large-scale datasets, especially suitable for urban planning and geographic spatial analysis.',
       site_url: 'https://pydeck.gl/',
@@ -106,7 +106,7 @@ export const Week2: IGroup = {
       name: 'WhiteboxTools',
       en_name: 'WhiteboxTools',
       description:
-        'WhiteboxTools is an advanced geospatial data analysis toolkit developed by the University of Guelph. It is used to perform GIS tasks, including terrain and hydrological analysis.',
+        'WhiteboxTools是一个由Guelph大学开发的高级地理空间数据分析工具集，用于执行包括地形和水文分析在内的GIS任务。',
       en_description:
         'WhiteboxTools is an advanced geospatial data analysis toolkit developed by the University of Guelph. It is used to perform GIS tasks, including terrain and hydrological analysis.',
       site_url: 'https://www.whiteboxgeo.com/',
@@ -118,7 +118,7 @@ export const Week2: IGroup = {
       name: 'H3',
       en_name: 'H3',
       description:
-        'H3 is a hexagonal hierarchical geospatial indexing system created by Uber. It is used to optimize geospatial analysis, such as gridding datasets and creating more uniform spatial sampling.',
+        'H3是由Uber创建的六边形层次地理编码系统，用于优化地理空间分析，例如网格化数据集和创建更均匀的地理空间采样。',
       en_description:
         'H3 is a hexagonal hierarchical geospatial indexing system created by Uber. It is used to optimize geospatial analysis, such as gridding datasets and creating more uniform spatial sampling.',
       site_url: 'https://h3geo.org/',
@@ -130,7 +130,7 @@ export const Week2: IGroup = {
       name: 'GDAL',
       en_name: 'GDAL',
       description:
-        'GDAL is an open-source library for reading and writing various spatial data formats. It provides powerful tools for spatial data transformation and processing.',
+        'GDAL是一个用于读取和写入多种空间数据格式的开源库，提供了强大的工具集，用于空间数据转换和处理。',
       en_description:
         'GDAL is an open-source library for reading and writing various spatial data formats. It provides powerful tools for spatial data transformation and processing.',
       site_url: 'https://gdal.org/',
